aaca5cd437ce48816105bc7ff7bcce6434d4344f
Naim <naimserin@gmail.com>
2015-05-25 11:17:33 +0300

    Control attr add js validate

--- Src/js/validation.js
+++ Src/js/validation.js
@@ -13,61 +13,145 @@
                 return true;
             }
         }, options);
+
+        var items = [];
+        $.each($("" + content + " [data-status]"), function () {
+            $(this).parent().append('<label class="control-label"></label>');
+            items.push({
+                item: $(this),
+                datatype: $(this).attr("data-type"),
+                datamintextvalue: $(this).attr("data-min-text-value"),
+                dataerrormessage: $(this).attr("data-error-message")
+            });
+        });
+
         $("" + content + " " + settings.button + "").click(function () {
             settings.onSubmit.call();
-            if ($("" + content + " [data-status='0']").val() != undefined) {
-                $.each($("" + content + " [data-status]"), function () {
-                    if ($(this).attr('data-status') == 0) {
-                        $(this).parent().children(':last').text($(this).attr('data-error-message'));
-                        $(this).next(".control-label").show("slow");
-                    } else {
-                        $(this).next(".control-label").hide("slow");
+            var totalError = 0;
+            $.each(items, function (i, val) {
+
+                if (val.item.attr('data-status') == undefined || val.item.attr('data-status') != 0 || val.item.attr('data-status') != 1) val.item.attr({ 'data-status': '0' });                
+                if (val.item.attr('data-type') == undefined || val.item.attr('data-type') != val.datatype)                    val.item.attr({ 'data-type': val.datatype }); 
+                if (val.item.attr('data-min-text-value') == undefined || val.item.attr('data-min-text-value') != val.datamintextvalue) val.item.attr({ 'data-min-text-value': val.datamintextvalue });
+                if (val.item.attr('data-error-message') == undefined || val.item.attr('data-error-message') != val.dataerrormessage) val.item.attr({ 'data-error-message': val.dataerrormessage });
+
+                if (val.datatype == 'email') {
+                    if ($.datatypeemail(val.item) == false) {
+                        totalError += 1;
                     }
-                });
+                }
+
+                if (val.datatype == 'text') {
+                    if ($.datatypetext(val.item) == false) {
+                        totalError += 1;
+                    }
+                }
+                if (val.datatype == 'number') {
+                    if ($.datatypenumber(val.item) == false) {
+                        totalError += 1;
+                    }
+                }
+                if (val.datatype == 'date') {
+                    if ($.datatypedate(val.item) == false) {
+                        totalError += 1;
+                    }
+                }
+                if (val.datatype == 'time') {
+                    if ($.datatypetime(val.item) == false) {
+                        totalError += 1;
+                    }
+                }
+            });
+
+            if (totalError > 0) {
                 settings.onError.call();
             }
             else {
                 settings.onCompleted.call();
             }
         });
-        $.each($("" + content + " [data-status]"), function () {
-            $(this).parent().append('<label class="control-label"></label>');
-        });
-        $("" + content + " [data-status]").on('change blur', function () {
-            var min = $(this).attr('data-min-text-value');
+        $.datatypetext = function (item) {
+            var status = false;
+            var min = $(item).attr('data-min-text-value');
             if (min != undefined) {
-                var val = $(this).val();
+                var val = $(item).val();
                 if (min > val.length) {
-                    $(this).attr({ 'data-status': '0' });
-                    $(this).parent().children(':last').text($(this).attr('data-error-message'));
-                    $(this).next(".control-label").show();
+                    status = false;
+                    $(item).attr({ 'data-status': '0' });
+                    $(item).parent().children(':last').text($(item).attr('data-error-message'));
+                    $(item).next(".control-label").show("slow");
                 } else {
-                    $(this).attr({ 'data-status': '1' });
-                    $(this).parent().children(':last').text('');
-                    $(this).next(".control-label").hide();
+                    status = true;
+                    $(item).attr({ 'data-status': '1' });
+                    $(item).parent().children(':last').text('');
+                    $(item).next(".control-label").hide("slow");
                 }
             }
+            return status;
+        };
+        $("" + content + " [data-type='text']").on('change blur', function () {
+            $.datatypetext(this);
         });
-        $("" + content + " [data-type='number']").on('keypress', function (evt) {
+        var number = /^\d+$/;
+        $.datatypenumber = function (item) {
+            var status = false;
+            if (number.test($(item).val()) == false) {
+                var status = false;
+                $(item).attr({ 'data-status': '0' });
+                $(item).parent().children(':last').text($(item).attr('data-error-message'));
+                $(item).next(".control-label").show("slow");
+            } else {
+                var status = true;
+                $(item).attr({ 'data-status': '1' });
+                $(item).parent().children(':last').text('');
+                $(item).next(".control-label").hide("slow");
+            }
+            return status;
+        };
+        $("" + content + " [data-type='number']").on('keypress blur', function (evt) {
+            $.datatypenumber(this);
             var charCode = (evt.which) ? evt.which : evt.keyCode;
             if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57)) {
                 return false;
             }
             return true;
         });
-        $("" + content + " [data-type='email']").on('change blur', function (evt) {
-            var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-            if (reg.test($(this).val()) == false) {
-                $(this).attr({ 'data-status': '0' });
-                $(this).parent().children(':last').text($(this).attr('data-error-message'));
-                $(this).next(".control-label").show();
+        var email = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+        $.datatypeemail = function (item) {
+            console.log(email.test($(item).val()));
+            var status = false;
+            if (email.test($(item).val()) == false) {
+                status = false;
+                $(item).attr({ 'data-status': '0' });
+                $(item).parent().children(':last').text($(item).attr('data-error-message'));
+                $(item).next(".control-label").show("slow");
             } else {
-                $(this).attr({ 'data-status': '1' });
-                $(this).parent().children(':last').text('');
-                $(this).next(".control-label").hide();
+                status = true;
+                $(item).attr({ 'data-status': '1' });
+                $(item).parent().children(':last').text('');
+                $(item).next(".control-label").hide("slow");
             }
+            return status;
+        };
+        $("" + content + " [data-type='email']").on('change blur', function (evt) {
+            $.datatypeemail(this);
         });
         var date = /^([0-9]{2})\.([0-9]{2})\.([0-9]{4})$/;
+        $.datatypedate = function (item) {
+            var status = false;
+            if (date.test($(item).val()) == false) {
+                status = false;
+                $(item).attr({ 'data-status': '0' });
+                $(item).parent().children(':last').text($(item).attr('data-error-message'));
+                $(item).next(".control-label").show("slow");
+            } else if (date.test($(item).val()) == true) {
+                status = true;
+                $(item).attr({ 'data-status': '1' });
+                $(item).parent().children(':last').text('');
+                $(item).next(".control-label").hide("slow");
+            }
+            return status;
+        };
         $("" + content + " [data-type='date']").on('keypress blur', function (evt) {
             var charCode = (evt.which) ? evt.which : evt.keyCode;
             if (charCode == 46) {
@@ -101,16 +185,30 @@
                 if (date.test($(this).val()) == false) {
                     $(this).attr({ 'data-status': '0' });
                     $(this).parent().children(':last').text($(this).attr('data-error-message'));
-                    $(this).next(".control-label").show();
+                    $(this).next(".control-label").show("slow");
                 } else {
                     $(this).attr({ 'data-status': '1' });
                     $(this).parent().children(':last').text('');
-                    $(this).next(".control-label").hide();
+                    $(this).next(".control-label").hide("slow");
                 }
             }
             return true;
         });
         var time = /^([0-9]{2})\:([0-9]{2})$/;
+        $.datatypetime = function (item) {
+            var status = false;
+            if (time.test($(item).val()) == false) {
+                $(item).attr({ 'data-status': '0' });
+                $(item).parent().children(':last').text($(item).attr('data-error-message'));
+                $(item).next(".control-label").show("slow");
+            } else if (time.test($(item).val()) == true) {
+                status = true;
+                $(item).attr({ 'data-status': '1' });
+                $(item).parent().children(':last').text('');
+                $(item).next(".control-label").hide("slow");
+            }
+            return status;
+        };
         $("" + content + " [data-type='time']").on('keypress blur', function (evt) {
             var charCode = (evt.which) ? evt.which : evt.keyCode;
             if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57)) {
@@ -140,11 +238,11 @@
                 if (time.test($(this).val()) == false) {
                     $(this).attr({ 'data-status': '0' });
                     $(this).parent().children(':last').text($(this).attr('data-error-message'));
-                    $(this).next(".control-label").show();
+                    $(this).next(".control-label").show("slow");
                 } else {
                     $(this).attr({ 'data-status': '1' });
                     $(this).parent().children(':last').text('');
-                    $(this).next(".control-label").hide();
+                    $(this).next(".control-label").hide("slow");
                 }
             }
             return true;
